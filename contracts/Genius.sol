c78b8f776c24b28ffc9673bbb66c755274e23ea7
APEWInu <114559363+APEWInu@users.noreply.github.com>
2023-02-11 21:52:35 +0700

    Update contracts to use SafeERC20 and lower Solidity version
    - Update the Solidity version from `0.8.4` to `0.8.0`
    - Replace the ERC20 import with a SafeERC20 import
    - Set initial supply to 195 million
    - Set monthly dev fund to 10 million
    
    [contracts/Genius.sol]
    - Replace the ERC20 import with a SafeERC20 import
    - Set initial supply to 195 million
    - Set monthly dev fund to 10 million
    [contracts/Migrations.sol]
    - Update the Solidity version from `0.8.4` to `0.8.0`

--- contracts/Genius.sol
+++ contracts/Genius.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.8.0;
 
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Taxable.sol";
 
-contract Genius is ReentrancyGuard, ERC20, AccessControl, Taxable  {
+contract Genius is ReentrancyGuard, SafeERC20, AccessControl, Taxable  {
     
     uint public constant INITIAL_SUPPLY = 195 * (10 ** 6) * (10 ** 18);
     uint public monthlyDevFund = (10 ** 7) * (10 ** 18);
